55bc87bf3f21a539b5faaa376647b197d3ccffb0
nana1981 <74463043+nana1981@users.noreply.github.com>
2022-01-24 22:25:52 +0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,36 @@
+package firstPackage;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        String str_1 ="nana";
+        System.out.println(str_1);
+//დავალება 1,2
+        int num_1 = 20;
+        int num_2 = 3;
+        int sum = num_1+num_2;
+        int sum1 = num_1 * num_2;
+        int sum2 = num_1 / num_2;
+        System.out.println(sum);
+        System.out.println(sum1);
+        System.out.println(sum2);
+        System.out.println(--sum1);
+
+
+        float myFloatnum1= 12.5f;
+        float myFloatnum2= 3.3f;
+
+        char a = '1', b = 'd', c = '@';
+
+//დავალება 3
+        int k= 5;
+        int y= 7;
+        boolean bol1 = k<5 || y>10;
+        System.out.println(bol1 + "+++");
+
+
+//დავალება 4
+
+
+    }
+}
---
+++ დავალება4.java
@@ -0,0 +1,13 @@
+package firstPackage;
+
+public class დავალება4 {
+    public static void main(String[] args) {String str_1 ="ჩემი დაბადების წელია";
+    String str_2 = "ჩემი ასაკია";
+
+    char a = ',';
+
+    int num_4= 1981;
+    int sum = 2022 - 1981;
+        System.out.println(str_1  +  num_4 + a+ str_2 + sum);
+}
+}
