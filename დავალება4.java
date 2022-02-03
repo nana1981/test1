9cc2d0c071bc061b0960bda041200a2649949db7
nana1981 <74463043+nana1981@users.noreply.github.com>
2022-02-03 23:54:00 +0400

    Update დავალება4.java

--- დავალება4.java
+++ დავალება4.java
@@ -1,13 +1,14 @@
 package firstPackage;
 
 public class დავალება4 {
-    public static void main(String[] args) {String str_1 ="ჩემი დაბადების წელია";
-    String str_2 = "ჩემი ასაკია";
+    public static void main(String[] args) {
+        String str_1 ="ჩემი დაბადების წელია";
+        String str_2 = "ჩემი ასაკია";
 
     char a = ',';
 
     int num_4= 1981;
     int sum = 2022 - 1981;
-        System.out.println(str_1  +  num_4 + a+ str_2 + sum);
+        System.out.println(str_1  + " " + num_4 + a + str_2 + " " + sum);
 }
 }
